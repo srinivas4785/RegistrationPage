c8e2c61707f291d4f42dd2fba68b32b449c117ef
srinivas4785 <68181688+srinivas4785@users.noreply.github.com>
2021-05-14 12:15:04 +0530

    contrnt change using javascript

--- scripts/practise.js
+++ scripts/practise.js
@@ -1,12 +1,31 @@
+var bikeNames = ["Royal Enfield Thunderbird", "Royal Enfield Himalayan."];
+
 var selector = document.getElementById("bikeselector");
 var bikeProfile = document.getElementById("bikeProfile");
 
-selector.addEventListener("change", () => {
-    console.log('new value ', selector.value);
-    if(selector.value == "Royal Enfield Bullet Trials."){
-        bikeProfile.src = "assets/dukebike.jfif";
+selector.addEventListener("change", onBikeNameChanged.bind(this));
+
+function onBikeNameChanged() {
+    parachange(selector.value);
+    changeBikeProp(selector.value);
+}
+ 
+function changeBikeProp (_bikename) {
+    for (let i = 0; i < bikeNames.length; i++) {
+        const bikeName = bikeNames[i];
+        if (_bikename === bikeName) {
+            console.log('_bikename :>> ', _bikename);   
+
+        }
     }
-    else{
-        bikeProfile.src = "assets/royal enfield.jpg";
+}
+function parachange (bikeName) {
+    var bikepara = document.getElementById("1stpara");
+    if (bikeName == bikeNames[0]) {
+        bikepara.textContent = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\nbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
     }
-})
+    else if ( bikeName == bikeNames[1]){
+        bikepara.textContent = "111111111111111111111111111111111111111111111\naaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\nbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
+
+    }
+}
